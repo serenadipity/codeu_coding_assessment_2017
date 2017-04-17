768332a3afc9ac7df5b1d283389a3df169b1b603
Serena Chan <cserena@umich.edu>
2017-04-17 15:13:18 -0400

    Compile errors still present, but basic logical framework established. Unable to fix errors because of time constraints

--- src/com/google/codeu/codingchallenge/MyJSON.java
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -15,40 +15,79 @@
 package com.google.codeu.codingchallenge;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
 
 final class MyJSON implements JSON {
+
+  private Map<String, JSON> jsonMap = new HashMap<>();
+
+  private String keyName;
+  private String strVal;
+  private JSON nestedObj;
+
 
   @Override
   public JSON getObject(String name) {
     // TODO: implement this
+    if (jsonMap.containsKey(name)) {
+        return jsonMap.get(name);
+    }
     return null;
   }
 
   @Override
   public JSON setObject(String name, JSON value) {
-    // TODO: implement this
+    if (jsonMap.containsKey(name)) {
+      nestedObj.setString(name, value);
+    }
+    else {
+      this.keyName = name;
+      this.nestedObj = value;
+      jsonMap.put(name, this);
+    }
     return this;
   }
 
+  //REQUIRES: value associated with given name must be a string
   @Override
   public String getString(String name) {
-    // TODO: implement this
+    if (jsonMap.containsKey(name)) {
+      //differentiated between string and object when called in JSONParser
+      if ((jsonMap.get(name)).strVal != "") {
+        return (jsonMap.get(name)).strVal;
+      }
+    }
     return null;
   }
 
   @Override
   public JSON setString(String name, String value) {
-    // TODO: implement this
+    if (jsonMap.containsKey(name)) {
+      strVal = value;
+    }
+    else {
+      this.keyName = name;
+      this.strVal = value;
+      jsonMap.put(name, this);
+    }
     return this;
   }
 
   @Override
   public void getObjects(Collection<String> names) {
     // TODO: implement this
+    for (int i=0; i < jsonMap.size(); i++) {
+      names.add(getObject(jsonMap.get(i)).keyName);
+    }
   }
 
   @Override
   public void getStrings(Collection<String> names) {
     // TODO: implement this
+    for (int i=0; i < jsonMap.size(); i++) {
+      names.add(getString(jsonMap.get(i)).keyName);
+    }
+
   }
 }
--- src/com/google/codeu/codingchallenge/MyJSONParser.java
+++ src/com/google/codeu/codingchallenge/MyJSONParser.java
@@ -20,7 +20,96 @@ final class MyJSONParser implements JSONParser {
 
   @Override
   public JSON parse(String in) throws IOException {
-    // TODO: implement this
-    return new MyJSON();
+    
+    /*******UTILIZE THESE FUNCTION FROM MyJSON.java********/
+  	//setObject(String name, JSON value);
+  	//setString(String name, String value);
+
+
+  	/********CONDITIONS TO CHECK FOR VALID INPUT**********/
+  	//check if the first non-whitespace character is a "{"
+  	//check if the next non-whitespace character is a ' " '
+  	//check if once you encounter the quotes, you have a valid string (if you have a quote you must have the backslash) + must have an endquote
+  	//must contain ":"
+  	//after the ":" must either have quotes or brackets
+
+  	MyJSON newObj = new MyJSON();
+  	MyJSON nestedObject = new MyJSON();
+  	String jsonName = "";
+  	
+
+  	//remove all whitespace for sequential parsing
+  	in.replaceAll(" ","");
+
+  	char[] temp = in.toCharArray();
+  	//to go through the string sequentially and check for correct format
+  	int parseIndex = 0;
+
+  	if (parseIndex != '{' || temp[temp.length] != '}') {
+  		throw new IOException("Must be valid JSON object: {}");
+  	}
+  	parseIndex++;
+  	if (temp.length > 2) {
+  		if (parseIndex != '\"') {
+  			throw new IOException("Key value must be type string");
+  		}
+  		else {
+  			parseIndex++;
+  			for (int i=2; i < (temp.length + 1); i++) {
+  				if (temp[i] == '\"') {
+  					if (i == parseIndex) {
+  						throw new IOException("key value must not be an empty string");
+  					}
+  					else {
+  						parseIndex = i;
+  					}
+  					i = temp.length + 1;
+  				}
+  			}
+  			jsonName = in.substring(2, parseIndex);
+  		}
+  	}
+  	if (temp.length > (parseIndex + 1)) {
+  		if ((temp[parseIndex + 1]) != ':') {
+  			throw new IOException("Must be in valid form Key : Value");
+  		}
+  		else {
+  			parseIndex += 2;
+  		}
+  	}
+  	if (temp.length > (parseIndex + 1)) {
+  		tempParseVal = parseIndex;
+  		boolean isValidObj = false;
+  		boolean isValidStr = false;
+  		if (temp[parseIndex] == '{') {
+  			while ((parseIndex + 1) < temp.length) {
+  				if (temp[parseIndex + 1] == '}') {
+  					isValidObj = true;
+  					MyJSON.setObject(parse(jsonName, in.substring(tempParseVal, parseIndex)));
+  				}
+  				parseIndex++;
+  			}
+  			if (!isValidObj) {
+  				throw new IOException("Must be valid object {}");
+  			}
+  		}
+  		else if (temp[tempParseVal] == '\"') {
+  			parseIndex = tempParseVal;
+  			while ((parseIndex + 1) < temp.length) {
+  				if (temp[parseIndex + 1] == '\"') {
+  					isValidStr = true;
+  					MyJSON.setString(jsonName, in.substring(tempParseVal, parseIndex));
+  				}
+  				parseIndex++;
+  			}
+  			if (!isValidStr) {
+  				throw new IOException("Must be valid string ");
+  			}
+  		}
+  	}
+
+
+
+    return newObj;
   }
 }
